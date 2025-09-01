e3ec9e0aa30b6f3999599047569e23774cfdde70
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-01 16:34:15 +0300

    feat(front): update app component

--- frontend-angular/ChatApp/src/app/app.routes.ts
+++ frontend-angular/ChatApp/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login';
+import { RegisterComponent } from './auth/register/register';
+import { ChangePasswordComponent } from './auth/change-password/change-password';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'auth/change-password', component: ChangePasswordComponent },
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth/login' }
+];
--- frontend-angular/ChatApp/src/app/app.ts
+++ frontend-angular/ChatApp/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LoginComponent } from "./auth/login/login";
+import { RegisterComponent } from "./auth/register/register";
+import { ChatWindowComponent } from './chat/chat-window/chat-window';
+import { ChatListComponent } from './chat/chat-list/chat-list';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, ChatWindowComponent,
+    ChatListComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
